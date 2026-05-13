ba24208491d63c440714c031ebfabb75b2cbdd47
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-05-14 04:13:51 +0530

    Basic server set up
    .

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const app = require('./src/app.js')
+const connectDB = require('./src/config/db.js')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+connectDB()
+
+app.listen(PORT, (req, res) => {
+    console.log(`Port started at ${PORT} `)
+})
