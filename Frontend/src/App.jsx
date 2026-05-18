e8981b45e05f3e31e741c87f5a0eb444a324a485
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-05-19 03:04:13 +0530

    Login, Register routes created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
-
+import {RouterProvider} from 'react-router'
+import {router} from './app.routes.jsx'
 
 function App() {
 
 
   return (
     <>
-      
+       <RouterProvider router={router} />
     </>
+
   )
 }
 
