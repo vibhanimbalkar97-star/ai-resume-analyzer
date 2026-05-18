c329dfc5504330ca6803ef459ad8000c91c76dcf
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-05-19 04:58:58 +0530

    Auth apis integration

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api/auth",
+  withCredentials: true,
+});
+
+// register
+export const register = async ({ username, email, password }) => {
+  try {
+    const response = await api.post("/register", {
+      username,
+      email,
+      password,
+    });
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// login
+export const login = async ({ email, password }) => {
+  try {
+    const response = await api.post("/login", {
+      email,
+      password,
+    });
+    response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// logout
+export const logout = async () => {
+  try {
+    const response = await api.get("/logout");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// get-me
+export const getMe = async () => {
+  try {
+    const response = api.get("/get-me");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
