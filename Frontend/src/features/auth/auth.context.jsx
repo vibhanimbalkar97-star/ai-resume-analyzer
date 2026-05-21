3cf57b33eead569da749c247a6fcfaa879e85849
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-05-21 09:09:09 +0530

    Interview report model done

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState } from "react";
-import { getMe } from "./services/auth.api";
+import { createContext, useState } from "react";
+
 
 export const AuthContext = createContext();
 
@@ -7,20 +7,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-    const getAndSetUser = async () => {
-      try {
-        const data = await getMe();
-        setUser(data.user);
-      } catch (err) {
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getAndSetUser();
-  }, []);
-
+ 
   return (
     <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
       {children}
--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,6 +1,6 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "./../auth.context.jsx";
-import { register, login, logout } from "./../services/auth.api";
+import { register, login, logout, getMe } from "./../services/auth.api";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -39,7 +39,22 @@ export const useAuth = () => {
     }
   };
 
+     useEffect(() => {
+    const getAndSetUser = async () => {
+      try {
+        const data = await getMe();
+        setUser(data.user);
+      } catch (err) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getAndSetUser();
+  }, []);
+
 
 
   return { user, loading, handleLogin, handleRegister, handleLogout };
 };
+
