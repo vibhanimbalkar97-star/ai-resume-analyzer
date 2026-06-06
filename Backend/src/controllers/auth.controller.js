e833d189b60bc78fc8d8e357c8700a092371ec0c
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-06-06 19:24:03 +0530

    changes done

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -95,7 +95,12 @@ const loginUserController = asyncHandler(async (req, res) => {
     },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", //only https
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
 
   res.status(200).json({
     message: "User loggedIn Successfully",
